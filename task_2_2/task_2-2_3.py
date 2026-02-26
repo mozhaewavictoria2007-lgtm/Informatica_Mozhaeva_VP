7ca0786e7e1837474d76f06a44da3a6dfbcbb865
Можаева Виктория <mozhaewavictoria2007@gmail.com>
2026-02-26 23:49:50 +0300

    Create task_2-2_3.py

---
+++ task_2_2/task_2-2_3.py
@@ -0,0 +1,6 @@
+thing_name = "Микроскоп"
+thing_num = 2718281828454540
+thing_cond = "Stable"
+thing_amount = 52
+
+print(f"Название:\t{thing_name}\nИнв.номер:\t{thing_num}\nCостояние:\t{thing_cond}\nКол-во:\t\t{thing_amount}")

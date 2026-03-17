fd0ceac35627d8736aa34077dcf20e770dcb9c23
Можаева Виктория <mozhaewavictoria2007@gmail.com>
2026-03-17 19:24:12 +0300

    Update task_2-4_lab_assistant.py

--- task_2_4/task_2-4_lab_assistant.py
+++ task_2_4/task_2-4_lab_assistant.py
@@ -1,1 +1,7 @@
-
+print ("ОТЧЁТ ПО ПРИГОТОВЛЕНИЮ:")
+print ("----------------------")
+common = float(input("Общий объем (мл):"))
+salt_mass = common * 0.009
+water = common
+print (f"Масса соли: {salt_mass:.2f} г ")
+print (f"Объем воды: {water} мл")
